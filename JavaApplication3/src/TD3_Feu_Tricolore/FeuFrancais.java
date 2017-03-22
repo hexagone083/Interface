e3ac4d6d817f0f560c0912a686ded87f284a4442
hexagone083 <florian@HexagonePC>
2017-03-22 20:13:41 +0100

    Début feu francais

--- JavaApplication3/src/TD3_Feu_Tricolore/FeuFrancais.java
+++ JavaApplication3/src/TD3_Feu_Tricolore/FeuFrancais.java
@@ -5,7 +5,11 @@
  */
 package TD3_Feu_Tricolore;
 
-import Tools.Tools;
+import Outils.Feu;
+import Outils.Tools;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
 
 /**
  *
@@ -18,7 +22,20 @@ public class FeuFrancais extends javax.swing.JFrame {
      */
     public FeuFrancais() {
         initComponents();
+
+        //Actions sur la fenêtre
         Tools.windowsInit(this);
+
+        //Initialisation de la vue
+        etat = Etat.Arret;
+        presentationArret();
+        eteindreToutLesFeu();
+
+        //Set de la couleur des feux
+        feuHaut.setCouleur_allume(Color.RED);
+        feuMilieu.setCouleur_allume(Color.ORANGE);
+        feuBas.setCouleur_allume(Color.GREEN);
+
     }
 
     /**
@@ -30,21 +47,346 @@ public class FeuFrancais extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        bouttonArret = new javax.swing.JButton();
+        feuHaut = new Outils.Feu();
+        feuMilieu = new Outils.Feu();
+        feuBas = new Outils.Feu();
+        bouttonMarche = new javax.swing.JButton();
+        bouttonPanne = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bouttonArret.setText("Arrêt");
+        bouttonArret.setToolTipText("");
+        bouttonArret.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bouttonArretActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout feuHautLayout = new javax.swing.GroupLayout(feuHaut);
+        feuHaut.setLayout(feuHautLayout);
+        feuHautLayout.setHorizontalGroup(
+            feuHautLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        feuHautLayout.setVerticalGroup(
+            feuHautLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout feuMilieuLayout = new javax.swing.GroupLayout(feuMilieu);
+        feuMilieu.setLayout(feuMilieuLayout);
+        feuMilieuLayout.setHorizontalGroup(
+            feuMilieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        feuMilieuLayout.setVerticalGroup(
+            feuMilieuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout feuBasLayout = new javax.swing.GroupLayout(feuBas);
+        feuBas.setLayout(feuBasLayout);
+        feuBasLayout.setHorizontalGroup(
+            feuBasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        feuBasLayout.setVerticalGroup(
+            feuBasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        bouttonMarche.setText("Marche");
+        bouttonMarche.setToolTipText("");
+        bouttonMarche.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bouttonMarcheActionPerformed(evt);
+            }
+        });
+
+        bouttonPanne.setText("Panne");
+        bouttonPanne.setToolTipText("");
+        bouttonPanne.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bouttonPanneActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(bouttonArret, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+                    .addComponent(bouttonMarche, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+                    .addComponent(bouttonPanne, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(57, 57, 57)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(feuHaut, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(feuMilieu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(feuBas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 740, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(feuHaut, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(feuMilieu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(feuBas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                .addComponent(bouttonMarche, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(bouttonArret, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(bouttonPanne, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    /*
+    Ci dessous les actions relativesqà la matrice Etat/Evènements
+    *******************************************************************************************************************************************************************************************************************************************
+     */
+    private void bouttonArretActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouttonArretActionPerformed
+
+        switch (etat) {
+            case Rouge:
+                etat = Etat.Arret;
+                presentationArret();
+                eteindreToutLesFeu();
+                break;
+            case Oange:
+                etat = Etat.Arret;
+                presentationArret();
+                eteindreToutLesFeu();
+                break;
+            case Vert:
+                etat = Etat.Arret;
+                presentationArret();
+                eteindreToutLesFeu();
+                break;
+            case OrangeAllume:
+                etat = Etat.Arret;
+                presentationArret();
+                eteindreToutLesFeu();
+                break;
+            case OrangeEtteint:
+                etat = Etat.Arret;
+                presentationArret();
+                eteindreToutLesFeu();
+                break;
+            case Arret:
+                //Interdit
+                break;
+
+        }
+
+    }//GEN-LAST:event_bouttonArretActionPerformed
+
+    private void bouttonMarcheActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouttonMarcheActionPerformed
+        switch (etat) {
+            case Rouge:
+                //Interdit
+                break;
+            case Oange:
+                //Interdit
+                break;
+            case Vert:
+                //Interdit
+                break;
+            case OrangeAllume:
+                etat = Etat.Rouge;
+                presentationRouge();
+                allumeFeu(feuHaut);
+                break;
+            case OrangeEtteint:
+                etat = Etat.Rouge;
+                presentationRouge();
+                allumeFeu(feuHaut);
+                break;
+            case Arret:
+                etat = Etat.Rouge;
+                presentationRouge();
+                allumeFeu(feuHaut);
+                break;
+
+        }
+
+    }//GEN-LAST:event_bouttonMarcheActionPerformed
+
+    private void bouttonPanneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouttonPanneActionPerformed
+        switch (etat) {
+            case Rouge:
+                break;
+            case Oange:
+                break;
+            case Vert:
+                break;
+            case OrangeAllume:
+                break;
+            case OrangeEtteint:
+                break;
+            case Arret:
+                break;
+
+        }
+
+    }//GEN-LAST:event_bouttonPanneActionPerformed
+
+    private void tickTimerRouge(ActionEvent e) {
+        switch (etat) {
+            case Rouge:
+                etat = Etat.Vert;
+                presentationVert();
+                allumeFeu(feuBas);
+                break;
+            case Oange:
+                //Interdit
+                break;
+            case Vert:
+                //Interdit
+                break;
+            case OrangeAllume:
+                //Interdit
+                break;
+            case OrangeEtteint:
+                //Interdit
+                break;
+            case Arret:
+                //Interdit
+                break;
+
+        }
+    }
+
+    private void tickTimerOrange(ActionEvent e) {
+        switch (etat) {
+            case Rouge:
+                break;
+            case Oange:
+                break;
+            case Vert:
+                break;
+            case OrangeAllume:
+                break;
+            case OrangeEtteint:
+                break;
+            case Arret:
+                break;
+
+        }
+    }
+
+    private void tickTimerVert(ActionEvent e) {
+        switch (etat) {
+            case Rouge:
+                break;
+            case Oange:
+                break;
+            case Vert:
+                break;
+            case OrangeAllume:
+                break;
+            case OrangeEtteint:
+                break;
+            case Arret:
+                break;
+
+        }
+    }
+
+    private void tickTimerOrangeAllume(ActionEvent e) {
+        switch (etat) {
+            case Rouge:
+                break;
+            case Oange:
+                break;
+            case Vert:
+                break;
+            case OrangeAllume:
+                break;
+            case OrangeEtteint:
+                break;
+            case Arret:
+                break;
+
+        }
+    }
+
+    private void tickTimerOrangeEtteint(ActionEvent e) {
+        switch (etat) {
+            case Rouge:
+                break;
+            case Oange:
+                break;
+            case Vert:
+                break;
+            case OrangeAllume:
+                break;
+            case OrangeEtteint:
+                break;
+            case Arret:
+                break;
+
+        }
+    }
+
+    /*
+    Ci dessous les  présentations coreespondantes
+    *******************************************************************************************************************************************************************************************************************************************
+     */
+    private void presentationArret() {
+        //Gestion des bouttons
+        Tools.activerBoutton(bouttonMarche);
+        Tools.desactiverBoutton(bouttonArret, bouttonPanne);
+
+        //Gestion des timmers
+        Tools.desactiverTimers(timer_rouge, timer_orange, timer_vert, timer_orange_allume, timer_orange_eteint);
+    }
+
+    private void presentationRouge() {
+        //Gestion des bouttons
+        Tools.activerBoutton(bouttonArret, bouttonPanne);
+        Tools.desactiverBoutton(bouttonMarche);
+
+        //Gestion des timmers
+        Tools.desactiverTimers(timer_orange, timer_vert, timer_orange_allume, timer_orange_eteint);
+        Tools.activerTimers(timer_rouge);
+    }
+
+    private void presentationVert() {
+        //Gestion des bouttons
+        Tools.activerBoutton(bouttonArret, bouttonPanne);
+        Tools.desactiverBoutton(bouttonMarche);
+
+        //Gestion des timmers
+        Tools.desactiverTimers(timer_rouge, timer_orange, timer_vert, timer_orange_allume, timer_orange_eteint);
+        Tools.activerTimers(timer_vert);
+    }
+
+    /*
+    Méthodes annexes
+     */
+    private void eteindreToutLesFeu() {
+        feuHaut.eteindre();
+        feuMilieu.eteindre();
+        feuBas.eteindre();
+    }
+
+    private void allumeFeu(Feu f) {
+        eteindreToutLesFeu();
+        f.allume();
+    }
 
     /**
      * @param args the command line arguments
@@ -81,6 +423,32 @@ public class FeuFrancais extends javax.swing.JFrame {
         });
     }
 
+    //Liste des états
+    private enum Etat {
+        Rouge, Oange, Vert, OrangeAllume, OrangeEtteint, Arret
+    }
+    Etat etat;
+
+    //Temps d'attents pour les timmers en ms
+    private final int temps_rouge = 1000;
+    private final int temps_orange = 1000;
+    private final int temps_vert = 1000;
+    private final int temps_orange_allume = 1000;
+    private final int temps_orange_etteint = 1000;
+
+    //Initialisation des timmers
+    private final Timer timer_rouge = new Timer(temps_rouge, this::tickTimerRouge);
+    private final Timer timer_orange = new Timer(temps_orange, this::tickTimerOrange);
+    private final Timer timer_vert = new Timer(temps_vert, this::tickTimerVert);
+    private final Timer timer_orange_allume = new Timer(temps_orange_allume, this::tickTimerOrangeAllume);
+    private final Timer timer_orange_eteint = new Timer(temps_orange_etteint, this::tickTimerOrangeEtteint);
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bouttonArret;
+    private javax.swing.JButton bouttonMarche;
+    private javax.swing.JButton bouttonPanne;
+    private Outils.Feu feuBas;
+    private Outils.Feu feuHaut;
+    private Outils.Feu feuMilieu;
     // End of variables declaration//GEN-END:variables
 }

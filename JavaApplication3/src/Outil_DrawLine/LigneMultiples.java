1c857ec89fb82ca94f2c5080ac794ac5af990459
hexagone083 <florian@HexagonePC>
2017-03-23 22:24:05 +0100

    Tp4 will be ended.

--- JavaApplication3/src/Outil_DrawLine/LigneMultiples.java
+++ JavaApplication3/src/Outil_DrawLine/LigneMultiples.java
@@ -17,16 +17,17 @@ import javax.swing.JPanel;
  */
 public class LigneMultiples extends JPanel {
 
-
     private int nombre_points;
-    private Stack<Point> liste = new Stack<>();
+    private final Stack<Point> liste = new Stack<>();
     private Color couleur;
     private Point point_temporaire;
+
     public void ajouterPoint(Point p) {
-      liste.add(p);
+        System.out.println("Add point. size: " + liste.size());
+        liste.add(p);
     }
 
-    public void supprimePremier(){
+    public void supprimePremier() {
         liste.pop();
     }
 
@@ -41,18 +42,31 @@ public class LigneMultiples extends JPanel {
     public void setCouleur(Color couleur) {
         this.couleur = couleur;
     }
-    
+
+    public void setPoint_temporaire(Point point_temporaire) {
+        this.point_temporaire = new Point(point_temporaire);
+    }
+      public void sauvegarde() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(couleur);
-        for (int i = 0; i < liste.size() -1; i++) {
-           g.drawLine(liste.get(i).x, liste.get(i).y, liste.get(i - 1).x, liste.get(i - 1).y);
+        for (int i = 0; i < liste.size() - 1; i++) {
+            g.drawLine(liste.get(i).x, liste.get(i).y,
+                    liste.get(i + 1).x, liste.get(i + 1).y);
         }
-       
+        //Tracage de la ligne temporaire
+        if (point_temporaire != null) {
+            g.drawLine(liste.lastElement().x, liste.lastElement().y,
+                    point_temporaire.x, point_temporaire.y);
+        }
+        point_temporaire = null;
 
     }
-    
+
+  
 
 }
